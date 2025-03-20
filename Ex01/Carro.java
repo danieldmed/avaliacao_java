1ddd40061b82be8393a565bbe4158786943b4d16
Daniel Duarte Medeiros <duartedaniel.contato@gmail.com>
2025-03-20 11:39:42 -0300

    Add files via upload

---
+++ Ex01/Carro.java
@@ -0,0 +1,13 @@
+public class Carro {
+    
+    String modelo;
+    String cor;
+    int velocidade;
+
+    Carro(String modelo, String cor, int velocidade) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.velocidade = velocidade;
+        
+    }
+}
---
+++ Ex01/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Carro carro1 = new Carro ("Uno", "Branco", 120);
+        String modelo = "Uno";
+        String cor = "Branco";
+        int velocidade = 120;
+
+        System.out.println("Modelo: " + modelo + " Cor: " + cor + " Velocidade: " + velocidade + " km/h");
+    }
+}
